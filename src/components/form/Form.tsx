8fa89b3b53137ba4d027037b8d2061a4d964a738
Sheyla <sheylapdv@gmail.com>
2023-08-16 20:02:17 +0200

    saveTask

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,51 +1,62 @@
 import React, { useState } from "react";
+import Axios from "axios";
 
 interface Task {
-  Description: string;
+  descripcion: string;
 }
 
 export default function Form() {
   const [task, setTask] = useState<Task>({
-    Description: "",
+    descripcion: "",
   });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setTask((prevTask) => ({
-      ...prevTask,
-      [name]: value,
-    }));
+  // const handleChange = (
+  //   e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  // ) => {
+  //   const { name, value } = e.target;
+  //   setTask((prevTask) => ({
+  //     ...prevTask,
+  //     [name]: value,
+  //   }));
+  // };
+
+  const saveTask = (): void => {
+    Axios.post("http://localhost:3001/v1/tasks", {
+      descripcion: task.descripcion,
+    })
+      .then(() => {
+        alert("Task created");
+      })
+      .catch((error) => {
+        console.error("Error al guardar la tarea:", error);
+      });
   };
 
-  const saveTask = async () => {
-    // Lógica para guardar la tarea
-  };
-
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    saveTask();
-  };
+  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   saveTask(task.descripcion);
+  // };
 
   return (
     <div className='card'>
       <div className='card-header'>Add Task</div>
       <div className='card-body'>
-        <form action='' onSubmit={onSubmit}>
+        <form action=''>
           <div className='form-group mb-3'>
-            <textarea
-              onChange={handleChange}
-              name='Description'
+            <input
+              onChange={(e) =>
+                setTask({ ...task, descripcion: e.target.value })
+              }
+              name='descripcion'
               placeholder='Task content'
               className='form-control'
-              cols={30}
-              rows={10}
-            ></textarea>
+              value={task.descripcion}
+            ></input>
           </div>{" "}
           <input className='form-control' placeholder='Filter'></input>
           <button
-            type='submit'
+            onClick={() => saveTask()}
+            type='button'
             className='btn btn-outline-success btn-sm btn-block'
           >
             Add Task
